638fb49bab4f0086b302b9ec8d602e30e827d54d
Darthclay <150842079+Darthclay@users.noreply.github.com>
2023-11-14 21:13:34 +0300

    Update db.php

--- db.php
+++ db.php
@@ -11,7 +11,8 @@ $con = mysqli_connect($serverName,$userName,$password,$dbName);
 if (mysqli_connect_errno()) {
 	echo "Failed to connect!";
 	exit();
-}
+}    echo "Connection success!";         
+	
 
  
  ?>
